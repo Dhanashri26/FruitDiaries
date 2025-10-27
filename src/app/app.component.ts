4025e4ded3daaa3b3597e567dae8ea4d1a55f5e4
Dhanashri26 <medhanashri26@gmail.com>
2025-10-27 20:11:54 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BlogService } from './blog.service';
+import { Meta, Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+
+  constructor(private meta: Meta, private titleService: Title) {}
+  
+  ngOnInit(): void {
+    this.titleService.setTitle('Fruit Diaries - Buy Fresh Fruits Online');
+
+     this.meta.addTags([
+      { name: 'description', content: 'Buy fresh fruits online at Fruit Diaries.' },
+      { property: 'og:title', content: 'Fruit Diaries - Fresh Fruits Delivered' },
+      { property: 'og:description', content: 'Buy fresh fruits online at affordable prices.' },
+      { property: 'og:image', content: '"https://images.unsplash.com/photo-1471500466955-85aecf33f71f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+      { property: 'og:url', content: 'http://localhost:4200/blog/1' },
+      
+    ]);
+  }
+  // title = 'fruitdiaries';
+
+ 
+}
+
+   
+
---
+++ src/app/blog.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Blog } from './models/Blog.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService {
+  
+  setTitle(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor(private http:HttpClient) { }
+
+  getAllBlogs() {
+     return this.http.get<Blog[]>("http://localhost:5000/api/blogs")
+  }
+  getBlogById(id:string) {
+     return this.http.get<Blog>(`http://localhost:5000/api/blogs/${id}`)
+  }
+}
