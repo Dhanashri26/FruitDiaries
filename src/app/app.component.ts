2aae7098941e572f3b55ab9a118e75baec1d6854
Dhanashri26 <medhanashri26@gmail.com>
2025-11-03 21:17:45 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Meta, Title } from '@angular/platform-browser';
+import { Meta, Title,BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  providers:[Meta],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,14 +18,14 @@ export class AppComponent implements OnInit{
     this.titleService.setTitle('Fruit Diaries - Buy Fresh Fruits Online');
     console.log("app initialized");
 
-    // this.meta.addTags([
-    //   { name: 'description', content: 'Buy fresh fruits online at Fruit Diaries.' },
-    //   { property: 'og:title', content: 'Fruit Diaries - Fresh Fruits Delivered' },
-    //   { property: 'og:description', content: 'Buy fresh fruits online at affordable prices.' },
-    //   { property: 'og:image', content: '"https://images.unsplash.com/photo-1471500466955-85aecf33f71f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-    //   { property: 'og:url', content: 'http://localhost:4200/blog/1' },
+    this.meta.addTags([
+      { name: 'description', content: 'Buy fresh fruits online at Fruit Diaries.' },
+      { property: 'og:title', content: 'Fruit Diaries - Fresh Fruits Delivered' },
+      { property: 'og:description', content: 'Buy fresh fruits online at affordable prices.' },
+      { property: 'og:image', content: '"https://images.unsplash.com/photo-1471500466955-85aecf33f71f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+      { property: 'og:url', content: 'http://localhost:4200/blog/1' },
       
-    // ]);
+    ]);
   }
   // title = 'fruitdiaries';
 }
